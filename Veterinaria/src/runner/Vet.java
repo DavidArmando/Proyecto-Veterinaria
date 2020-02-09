f492890b48d88b36adf13bad4aa45b9ab8480908
Andrés Felipe Chaparro Rosas <36827424+AFelipeCR@users.noreply.github.com>
2020-02-08 22:14:05 -0500

    Add files via upload

--- Veterinaria/src/runner/Vet.java
+++ Veterinaria/src/runner/Vet.java
@@ -2,18 +2,18 @@ package runner;
 
 import java.io.IOException;
 
-import controller.Controller;
 import general.HandlerLanguage;
 import view.MyFrame;
 
 public class Vet {
 	public static void main(String[] args) {
-		new Controller();
+		
 		try {
 			HandlerLanguage.getInstance().loadLanguage("ES");
 		} catch (IOException e) {
 			System.err.println(e.getStackTrace());
 		}
+		
 		new MyFrame().startFrame();
 	}
 }
